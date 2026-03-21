ef2d6307f748e9838adccfe6aaad524b8345cd5d
Utkarsha  Naik <132172238+UtkarshaGN@users.noreply.github.com>
2026-03-21 22:44:09 +1000

    array methods- pop, push, unshift, shift, join

---
+++ Array methods/app.js
@@ -0,0 +1,32 @@
+//Array - arrays store multiple values into single variable
+//Array methods- push(), pop(), unshift(), shift(), join()
+
+//push()- Add to end
+// let l = [10, 20, 33, 45, 56]
+// l.push(30)
+// l.push(65)
+// console.log(l);
+
+//pop()- remove last element
+// let arr = [22, 45, 67, 78, 45]
+// arr.pop()
+// console.log(arr);
+// arr.pop()
+// console.log(arr)
+
+//unshift()- add to start
+// let n = [22, 33, 44, 55]
+// n.unshift(11)
+// console.log(n)
+
+//shift()- remove last element
+
+// let m = [34, 67, 98, 99 , 70]
+// m.shift()
+// console.log(m)
+
+//join()- converts array into string
+
+let names = ["Radha", "Avya", "Hana", "Aayu"]
+let joinNames = names.join(" - ")
+console.log(joinNames)
