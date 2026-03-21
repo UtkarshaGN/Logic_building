1b0ffbc47abed92ef5299d98ba43d6166a5578cc
Utkarsha  Naik <132172238+UtkarshaGN@users.noreply.github.com>
2026-03-21 23:04:42 +1000

    for_of and for_in loop

---
+++ special_loops/app.js
@@ -0,0 +1,24 @@
+//for_of loop - it will give values of array and string.
+
+// let arr= [10, 20, 30, 40, 50]
+
+// for(let res of arr){
+//     console.log(res)
+// }
+
+// let str = "hello";
+// for(result of str){
+//     console.log(result)
+// }
+
+//for_in loop - it will give index number of array, string and object
+
+// let str= "welcome";
+// for(let res in str){
+//     console.log(res, str[res])
+// }
+
+let arr = [22, 45, 67, 89,90]
+for(let result in arr){
+    console.log(result, arr[result])
+}
